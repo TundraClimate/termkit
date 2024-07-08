9cafba37e394cea5fc7511d0c4444ff487320aa9
TundraClimate <tundra.tundere@gmail.com>
2024-07-08 22:44:38 +0900

    ✨feat: add tui manager

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    execute,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
+
 mod event_thread;
 pub use event_thread::EventThread;
 
 pub mod render;
+
+pub fn enable_tui() -> io::Result<()> {
+    execute!(io::stdout(), EnterAlternateScreen, Hide)?;
+    terminal::enable_raw_mode()?;
+    Ok(())
+}
+
+pub fn disable_tui() -> io::Result<()> {
+    execute!(io::stdout(), LeaveAlternateScreen, Show)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
