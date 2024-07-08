7bff117255709b0fc01e338e5bc33055d131664e
TundraClimate <tundra.tundere@gmail.com>
2024-07-08 21:51:10 +0900

    ✨feat: update sample_app

--- examples/sample_app.rs
+++ examples/sample_app.rs
@@ -1,12 +1,19 @@
-use crossterm::event::{Event, KeyCode, KeyEventKind};
+use crossterm::{
+    event::{Event, KeyCode, KeyEventKind},
+    style::Color,
+    terminal,
+};
 use std::error::Error;
-use termkit::EventThread;
+use termkit::{render, EventThread};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut ev = EventThread::spawn();
 
     loop {
+        let (_, rows) = terminal::size()?;
+        render::horizontal_bar(0, Color::White)?;
+        render::horizontal_bar(rows, Color::White)?;
         if let Ok(event) = ev.read() {
             if let Event::Key(event) = event {
                 if event.kind == KeyEventKind::Press {
